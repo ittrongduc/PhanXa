3f35bb02606c0dd57ae4297736af7686f7e0d47e
duc.nt <ittrongduc@gmail.com>
2019-07-18 00:37:48 +0700

    [Sprint01][Feature] TextView event

--- app/src/main/java/com/zdzz/phanxa/MainActivity.java
+++ app/src/main/java/com/zdzz/phanxa/MainActivity.java
@@ -3,12 +3,35 @@ package com.zdzz.phanxa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+        textView = findViewById(R.id.textView);
+        textView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.textView){
+            textView.setText("DucNguyen");
+        }
+
     }
 }
