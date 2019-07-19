6e72baa67acfc25ed84bfdb36328afc5a774045d
duc.nt <ittrongduc@gmail.com>
2019-07-20 03:05:17 +0700

    [Sprint01][Feature] Source Push

--- app/src/main/java/com/zdzz/phanxa/MainActivity.java
+++ app/src/main/java/com/zdzz/phanxa/MainActivity.java
@@ -7,30 +7,57 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.zdzz.phanxa.service.time.TimeService;
+import com.zdzz.phanxa.service.time.impl.TimeServiceImpl;
+
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Context
     private TextView textView;
+
+    //Service
+    private TimeService timeService;
+
+    //Model
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initService();
         initView();
+
+        doGetMilliSecondsTime();
+    }
+
+    private void doGetMilliSecondsTime() {
+        textView.setOnClickListener(this);
+    }
+
+    private void initService() {
+        timeService = new TimeServiceImpl();
     }
 
     private void initView() {
-
         textView = findViewById(R.id.textView);
-        textView.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View view) {
 
         if (view.getId() == R.id.textView){
-            textView.setText("NguyenTrongDuc");
+            try {
+                if (Long.getLong(textView.getText().toString()) instanceof  Long){
+                    textView.setText(new Date(Long.parseLong(textView.getText().toString())).toString());
+                }
+                long millisecondTime = timeService.getMillisecondTime();
+                textView.setText(String.valueOf(millisecondTime));
+            }catch (Exception e){
+
+            }
         }
 
     }
